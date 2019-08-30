1d333aaa44858f0338145a8364c9af11fcdb71d8
Will Saymon <wsaymonoficial@gmail.com>
2019-08-30 16:16:37 -0300

    [ADD] Safe box

---
+++ Assets/SafeBoxController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SafeBoxController : MonoBehaviour
+{
+    public GameObject safeBoxWindow;
+    public GameObject safeBox;
+    public GameObject openSafeBox;
+    public TextMeshProUGUI text;
+    public string correctPass;
+    public AudioClip buttonSound;
+    public AudioClip wrongSound;
+    public AudioClip openSound;
+
+    public AudioSource audioSource;
+    int length;
+    bool isOpen;
+    string currentPass = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        length = correctPass.Length * 2;
+        text.SetText(currentPass);
+    }
+
+    public void Button(string value)
+    {
+        audioSource.PlayOneShot(buttonSound);
+        currentPass += (value + " ");
+        if (currentPass.Length == length)
+        {
+            if (currentPass.Replace(" ", "") == correctPass)
+            {
+                isOpen = true;
+                audioSource.PlayOneShot(openSound);
+
+                openSafeBox.SetActive(true);
+                safeBox.SetActive(false);
+            }
+            else
+            {
+                currentPass = "";
+                audioSource.PlayOneShot(wrongSound);
+            }
+        }
+
+        text.SetText(currentPass);
+    }
+
+    public void Show()
+    {
+        safeBoxWindow.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        safeBoxWindow.SetActive(false);
+    }
+}
